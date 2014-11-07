c346c77a6cac74136063059d95d7e3e5a4c91ef1
Administrator <Administrator@alvis-PC>
2014-11-07 18:07:35 +0800

    项目提交

---
+++ src/main/java/com/code/app/AppMain.java
@@ -0,0 +1,13 @@
+package com.code.app;
+
+import org.apache.log4j.Logger;
+
+public class AppMain {
+	public static Logger logger=Logger.getLogger(AppMain.class);
+	
+	public static void main(String[] args) {
+		System.out.println("hello");
+		logger.info("------");
+
+	}
+}
