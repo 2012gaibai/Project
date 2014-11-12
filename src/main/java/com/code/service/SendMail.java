d5ffca70e47be541a14a64e5a4dce33c74090f41
Administrator <Administrator@alvis-PC>
2014-11-12 21:20:47 +0800

    项目

--- src/main/java/com/code/service/SendMail.java
+++ src/main/java/com/code/service/SendMail.java
@@ -1,10 +1,25 @@
 package com.code.service;
 
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import com.code.utils.SpringUtil;
+
 public class SendMail implements ISend {
 
+	//private String from;
+		
 	public void Send() {
-		System.out.println("邮件发送成功");
-
+		//System.out.println("邮件发送成功");
+		SimpleMailMessage smm=new SimpleMailMessage();
+		JavaMailSenderImpl mailSender=(JavaMailSenderImpl) SpringUtil.getBean("sendMail");		
+		smm.setFrom(mailSender.getUsername());
+		smm.setTo("daxue513@126.com");
+		smm.setSubject("测试");
+		smm.setText("发送成功");
+		mailSender.send(smm);
+		System.out.println("--------");
 	}
+	
 
 }
